e7778526e805299833fb326caade1c1c7316ab6f
mistim <m.mistim@gmail.com>
2018-10-22 17:19:22 +0300

    modified js

--- dist/js/wave.js
+++ dist/js/wave.js
@@ -1,4 +1,74 @@
+'use strict';
+
 import '../sass/wave.scss';
+
+class Wave {
+
+  constructor(event, opts) {
+    this.defaultOpts = {
+      element: event.currentTarget, // Элемент по которому кликали
+      type: 'hit', // Место растпространения волны
+      bgc: 'rgba(0, 0, 0, 0.15)' // Цвет пульсации
+    };
+    this.opts = Object.assign(this.defaultOpts, opts);
+
+    this.checkColor();
+    this.checkColor();
+  }
+
+  init() {
+    let target = this.opts.element;
+
+    if(target) {
+      let rect = target.getBoundingClientRect(),
+        wave = target.querySelector('.e-wave'),
+        top = 0,
+        left = 0;
+
+      if (!wave) {
+        wave = document.createElement('span');
+        wave.className = 'e-wave';
+        wave.style.height = wave.style.width = Math.max(rect.width, rect.height) + 'px';
+        target.appendChild(wave);
+      } else {
+        wave.className = 'e-wave';
+      }
+
+      switch (this.opts.type) {
+        case 'center':
+          top = rect.height / 2;
+          left = rect.width / 2;
+          break;
+
+        default:
+          top = event.layerY;
+          left = event.layerX;
+      }
+
+      wave.style.top = (top - wave.offsetHeight / 2 ) + 'px';
+      wave.style.left = (left - wave.offsetWidth / 2) + 'px';
+      wave.style.backgroundColor = this.opts.bgc;
+      wave.className = 'e-wave z-active';
+    }
+  }
+
+  checkType() {
+    if (this.opts.element.classList.contains('wave-center')) {
+      this.opts.type = 'center';
+    }
+  }
+
+  checkColor() {
+    switch (true) {
+      case this.opts.element.classList.contains('wave-white'):
+        this.opts.bgc = 'rgba(255, 255, 255, 0.3)';
+        break;
+
+      default:
+        this.opts.bgc = this.defaultOpts.bgc;
+    }
+  }
+}
 
 /**
  * Эффект пульсации кнопки
@@ -7,48 +77,12 @@ import '../sass/wave.scss';
  * @param arg_opts
  * @returns {boolean}
  */
-export default function(event, arg_opts) {
-    let opts = Object.assign({
-            element: event.currentTarget, // Элемент по которому кликали
-            type: 'hit', // Место растпространения волны
-            bgc: 'rgba(0, 0, 0, 0.15)' // Цвет пульсации
-        }, arg_opts),
-        target = opts.element;
+export default function(element, opts) {
+  let elements = document.querySelectorAll(element);
 
-    if (target.classList.contains('wave-center')) {
-        opts.type = 'center';
-    }
-
-    if(target) {
-        let rect = target.getBoundingClientRect(),
-            wave = target.querySelector('.e-wave'),
-            top = 0,
-            left = 0;
-
-        if (!wave) {
-            wave = document.createElement('span');
-            wave.className = 'e-wave';
-            wave.style.height = wave.style.width = Math.max(rect.width, rect.height) + 'px';
-            target.appendChild(wave);
-        } else {
-            wave.className = 'e-wave';
-        }
-
-        switch (opts.type) {
-            case 'center':
-                top = rect.height / 2;
-                left = rect.width / 2;
-                break;
-            default:
-                top = event.layerY;
-                left = event.layerX;
-        }
-
-        wave.style.top = (top - wave.offsetHeight / 2 ) + 'px';
-        wave.style.left = (left - wave.offsetWidth / 2) + 'px';
-        wave.style.backgroundColor = opts.bgc;
-        wave.className = 'e-wave z-active';
-
-        return false;
-    }
+  Array.prototype.forEach.call(elements, function(item) {
+    item.addEventListener('click', function (event) {
+      new Wave(event, opts).init();
+    });
+  });
 }
